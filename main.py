e25a80b8019ff9743191a97d24f1ae25fb5f8c2b
JiaJiunn <ja497@cornell.edu>
2021-05-10 14:40:27 -0400

    completed cost maps

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ PARENT_DIR = Path(__file__).parent.parent
 HUMAN_RADIUS = 3
 HUMAN_DIR_LEN = 10
 HUMAN_DIR_ANGLE_LEN = 15
+HUMAN_DIR_ANGLE_LEN_BLOCKED = 30
 STANDING_SAFETY = 13 # NOTE MUST BE ODD "less threatened when standing"
 STANDING_KERNEL_SIZE = 3
 SITTING_SAFETY = 21 # NOTE MUST BE ODD
 SITTING_KERNEL_SIZE = 3
+VISIBILITY_RATIO = -1
+AWARENESS_KERNEL_SIZE = 1
+INTERRUPT_COST_RATIO = 0.7
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
@@ -177,11 +181,137 @@ def get_visibility_cost(map_dict): # TODO invert costs (is currently reward, *-1
 			cc = cc[in_bounds]
 			
 			# TODO check if vector has been plotted before
-			map_mat_copy[rr, cc] = get_normal_red(len(map_mat[rr, cc]), 1)
+			map_mat_copy[rr, cc] = get_normal_red(len(map_mat[rr, cc]), 1) * VISIBILITY_RATIO
 
 		map_mat += map_mat_copy # important for overlaps
 		
 	return map_mat
+
+
+def get_hidden_zone_cost(map_dict):
+	map_size = map_dict['map_size']
+	map_mat = get_obs_cost(map_dict)
+	map_mat_empty = np.zeros((map_size, map_size))
+
+	for hum in map_dict['humans']:
+		x, y = hum['coordinates']
+
+		dir_angle = hum['direction']
+		map_mat_copy = np.zeros((map_size, map_size))
+		for angle in range(dir_angle-45, dir_angle+45):
+
+			# gets angle
+			if angle >= 360:
+				angle -= 360
+			if angle < 0:
+				angle += 360
+
+			# gets line from angle
+			x_diff, y_diff = angle_to_vec(angle)
+			x_diff, y_diff = round(x_diff * HUMAN_DIR_ANGLE_LEN_BLOCKED, 2), round(y_diff * HUMAN_DIR_ANGLE_LEN_BLOCKED, 2)
+			rr, cc = line(y, x, int(y+y_diff), int(x+x_diff))
+
+			# checks if cells are all within grid
+			in_bounds = []
+			for idx in range(len(rr)):
+				if rr[idx] >= 0 and rr[idx] < map_size and cc[idx] >= 0 and cc[idx] < map_size:
+					in_bounds.append(True)
+				else:
+					in_bounds.append(False)
+			rr = rr[in_bounds]
+			cc = cc[in_bounds]
+
+			# TODO check if vector has been plotted before
+			vis_arr = np.zeros(len(rr))
+			vis_blocked = False
+			normal_red = get_normal_red(len(rr), AWARENESS_KERNEL_SIZE) # TODO
+			for idx in range(len(rr)):
+				if map_mat[rr[idx], cc[idx]] != 0:
+					vis_blocked = True
+				if vis_blocked:
+					vis_arr[idx] = normal_red[idx]
+			map_mat_copy[rr, cc] = vis_arr
+
+		map_mat_empty += map_mat_copy # important for overlaps
+
+	return map_mat_empty
+
+
+def euc_dist(point1, point2):
+    return np.linalg.norm(point1 - point2)
+
+
+def get_interrupt_cost(map_dict):
+    
+    map_mat = np.zeros((map_dict['map_size'], map_dict['map_size']))
+#     map_mat = get_obs_cost(map_dict)
+    
+    for interaction in map_dict['interactions']:
+        obs_idx, hum_idx = interaction['indices']
+
+        obs = map_dict['obstacles'][obs_idx]
+        hum = map_dict['humans'][hum_idx]
+
+        hum_x, hum_y = hum['coordinates']
+        tl_x, tl_y, br_x, br_y = obs['coordinates']
+
+        # get two closest edges of object
+        obs_edges = np.array([[tl_x, tl_y], [br_x, tl_y], [tl_x, br_y], [br_x, br_y]])
+        euc_dist_lst = []
+        for edge_coord in obs_edges:
+            euc_dist_lst.append(euc_dist((hum_x, hum_y), edge_coord))
+        first_edge_idx, second_edge_idx = np.argpartition(euc_dist_lst, 2)[:2]
+
+        dir_vec_1 = np.array([obs_edges[first_edge_idx][1] - hum_y, obs_edges[first_edge_idx][0] - hum_x])
+        dir_vec_1_normalized = dir_vec_1/np.linalg.norm(dir_vec_1)
+        deg_1 = np.degrees(np.arctan2(dir_vec_1_normalized[0], dir_vec_1_normalized[1])) + 1
+        if deg_1 < 0:
+            deg_1 += 360
+
+        dir_vec_2 = np.array([obs_edges[second_edge_idx][1] - hum_y, obs_edges[second_edge_idx][0] - hum_x])
+        dir_vec_2_normalized = dir_vec_2/np.linalg.norm(dir_vec_2)
+        deg_2 = np.degrees(np.arctan2(dir_vec_2_normalized[0], dir_vec_2_normalized[1])) + 1
+        if deg_2 < 0:
+            deg_2 += 360
+
+        # TODO degree edge case
+        if deg_2 < deg_1:
+            deg_1, deg_2 = deg_2, deg_1
+            
+        interrupt_len = max(np.linalg.norm(dir_vec_1), np.linalg.norm(dir_vec_2))
+        for angle in range(int(deg_1), int(deg_2)):
+            x_diff, y_diff = angle_to_vec(angle)
+            x_diff, y_diff = round(x_diff * interrupt_len, 2), round(y_diff * interrupt_len, 2)
+            rr, cc = line(hum_y, hum_x, int(hum_y+y_diff), int(hum_x+x_diff))
+
+            # checks if cells are all within grid
+            in_bounds = []
+            for idx in range(len(rr)):
+                if rr[idx] >= 0 and rr[idx] < map_dict['map_size'] and cc[idx] >= 0 and cc[idx] < map_dict['map_size']:
+                    in_bounds.append(True)
+                else:
+                    in_bounds.append(False)
+            rr = rr[in_bounds]
+            cc = cc[in_bounds]
+
+            map_mat[rr, cc] = np.ones(len(map_mat[rr, cc])) * INTERRUPT_COST_RATIO
+    
+    return map_mat
+
+
+# for visualizations
+def overlay_obs_hum(map_dict, cost_map):
+
+	for obs in map_dict['obstacles']:
+		tl_x, tl_y, br_x, br_y = obs['coordinates']
+		obs_shape = (br_y-tl_y+1, br_x-tl_x+1)
+		cost_map[tl_y:(br_y+1), tl_x:(br_x+1)] = np.ones(obs_shape)
+
+	for hum in map_dict['humans']:
+		x, y = hum['coordinates']
+		cost_map[y, x] = 1
+
+	return cost_map
 
 
 def main():
@@ -201,11 +331,15 @@ def main():
 	window_size = map_dict['map_size'] * screen_resolution
 	screen = pygame.display.set_mode((window_size, window_size))
 
-	# test_safety_cost = get_safety_cost(map_dict)
-	# test_visibility_cost = get_visibility_cost(map_dict)
-	# test_obs_cost = get_obs_cost(map_dict)
-	# plt.imshow(test_obs_cost, cmap='gray')
-	# plt.show()
+	# set up cost map
+	test_safety_cost = get_safety_cost(map_dict)
+	test_visibility_cost = get_visibility_cost(map_dict)
+	test_obs_cost = get_obs_cost(map_dict)
+	test_hidden_zone_cost = get_hidden_zone_cost(map_dict)
+	test_interrupt_cost = get_interrupt_cost(map_dict)
+	cost_map = test_safety_cost + test_visibility_cost + test_obs_cost + test_hidden_zone_cost + test_interrupt_cost
+	plt.imshow(overlay_obs_hum(map_dict, cost_map), cmap='gray')
+	plt.show()
 
 	while True:
 		screen.fill(WHITE)
